421c1b2b7cb85e7827bf74fc483b8f9b2fd4b492
Omar Zaki <omarhzaki@Hotmail.ca>
2025-11-20 09:17:52 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/flowerwall-vancouver-demo/',   // 👈 important for GitHub Pages
+  base: '/flowerwall-vancouver-demo/', // 👈 repo name here
 })
